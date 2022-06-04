3f208ccb0d8e8cae2612e77b3009169245155f5a
alexisarte <alexisrodriguezarteaga@gmail.com>
2022-06-04 12:36:22 -0300

    Mejora en la solución (objeto agrupador)

--- AyED/src/tp02/ejercicio4/TestBalanceo.java
+++ AyED/src/tp02/ejercicio4/TestBalanceo.java
@@ -8,19 +8,11 @@ public class TestBalanceo {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Ingrese la cadena a balancear: ");
-		String cadena = s.next();
-		boolean answer = balance(cadena);
+		System.out.print(evaluarBalance(s.next()) ? "Balanceada" : "Desbalanceada");
 		s.close();
-		
-		if (answer == true) {
-			System.out.println("Balanceada");
-		} else {
-			System.out.println("Desbalanceada");
-		}
-//		System.out.print(evaluarBalance(s.next()) ? "Balanceada" : "Desbalanceada");
 	}
-	
-	public static boolean balance(String cadena) {
+
+	public static boolean evaluarBalance(String cadena) {
 		Agrupador test = new Agrupador();
 		PilaGenerica<Character> apertura = new PilaGenerica<Character>();
 		int i = 0;
@@ -33,42 +25,20 @@ public class TestBalanceo {
 					apertura.apilar(caracter);
 				}
 				if (test.esCierre(caracter)) {
-					if (apertura.esVacia()) {
-						if (test.match(apertura.tope(), caracter)) {
-							apertura.desapilar();
+					if (!apertura.esVacia()) {
+						if (test.match(apertura.tope(), caracter)) {	// siempre hay match si la cadena esta balanceada
+							apertura.desapilar();						// porque desapilo el caracter de apertura correspondiente al de cierre
+						} else {									
+							balanceo = false;	// si no hay match, esta desbalanceada, dejo de evaluar la cadena
 						}
+					} else {
 						balanceo = false;
 					}
-					balanceo = false;
 				}
 			}
 			++i;
 		}
 		return apertura.esVacia() && (i == cadena.length());
-	}
-	
-	public static boolean evaluarBalance(String cadena) {
-		PilaGenerica<Character> pila = new PilaGenerica<Character>();
-		char caracter;
-		for (int i = 0; i < cadena.length(); i++) {
-			caracter = cadena.charAt(i);
-			if (caracter == '(' || caracter == '[' || caracter == '{') {
-				pila.apilar(caracter);
-			} else if (caracter == ')') {
-				if (pila.esVacia() || pila.desapilar() != '(') {
-					return false;
-				}
-			} else if (caracter == ']') {
-				if (pila.esVacia() || pila.desapilar() != '[') {
-					return false;
-				}
-			} else if (caracter == '}') {
-				if (pila.esVacia() || pila.desapilar() != '{') {
-					return false;
-				}
-			}
-		}
-		return pila.esVacia();
 	}
 
 }
