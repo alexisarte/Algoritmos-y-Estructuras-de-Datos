b437c8d38676b7e47c95e893486a8e67bff81de6
alexisarte <alexisrodriguezarteaga@gmail.com>
2022-07-21 18:40:11 -0300

    algoritmo topsort

--- AyED/src/tp06/ejercicio7/TestTopsort.java
+++ AyED/src/tp06/ejercicio7/TestTopsort.java
@@ -7,18 +7,16 @@ import tp06.ejercicio4.CargarGrafo;
 public class TestTopsort {
 
 	public static void main(String[] args) {
-		
 		Topsort topsort = new Topsort();
 		CargarGrafo cargar = new CargarGrafo();
 		Grafo<String> grafo = cargar.cargarGrafoDirigidoString();
+
 		ListaGenerica<String> lista = topsort.topsort(grafo);
 		System.out.println(lista.toString());
-		
 		System.out.println();
 		
 		lista = topsort.topsort2(grafo);
 		System.out.println(lista.toString());
-		
 		System.out.println();
 		
 		lista = topsort.topsortEnProfundidad(grafo);
--- AyED/src/tp06/ejercicio7/Topsort.java
+++ AyED/src/tp06/ejercicio7/Topsort.java
@@ -2,6 +2,7 @@ package tp06.ejercicio7;
 
 import tp02.ejercicio2.ListaEnlazadaGenerica;
 import tp02.ejercicio2.ListaGenerica;
+import tp02.ejercicio3.ColaGenerica;
 import tp02.ejercicio3.PilaGenerica;
 import tp06.ejercicio3.Arista;
 import tp06.ejercicio3.Grafo;
@@ -13,39 +14,51 @@ public class Topsort {
 	public ListaGenerica<String> topsort(Grafo<String> grafo) {
 		ListaGenerica<Vertice<String>> vertices = grafo.listaDeVertices();
 		ListaGenerica<String> lista = new ListaEnlazadaGenerica<>();
-		ListaGenerica<Arista<String>> adyacentes = null;
-		int[] grados = new int[vertices.tamanio() + 1];
+		ListaGenerica<Arista<String>> aristas = null;
+		int[] gradosDeEntrada = new int[vertices.tamanio() + 1];
 		Vertice<String> vertice = null;
-		this.calcularGrados(grados, vertices);
-		var pos = this.encontrarGradoCero(grados);
+		var numVerticesVisitados = 0;
+		this.calcularGradosDeEntrada(gradosDeEntrada, vertices);
+		var pos = this.encontrarGradoCero(gradosDeEntrada);
 		while (pos != -1) {
 			vertice = grafo.listaDeVertices().elemento(pos);
 			lista.agregarFinal(vertice.dato());
-			grados[pos] = -1;
-			adyacentes = grafo.listaDeAdyacentes(vertice);
-			this.decrementarGrado(grados, adyacentes);
-			pos = this.encontrarGradoCero(grados);
+			++numVerticesVisitados;
+			gradosDeEntrada[pos] = -1;
+			aristas = grafo.listaDeAdyacentes(vertice);
+			this.decrementarGrado(gradosDeEntrada, aristas);
+			pos = this.encontrarGradoCero(gradosDeEntrada);
 		}
-		return lista;
+		if (numVerticesVisitados != vertices.tamanio()) {
+			throw new IllegalArgumentException("El grafo no es acíclico");
+		} else {
+			return lista;
+		}
 	}
 
 	public ListaGenerica<String> topsort2(Grafo<String> grafo) {
-		PilaGenerica<Vertice<String>> pila = new PilaGenerica<>();
+		ColaGenerica<Vertice<String>> cola = new ColaGenerica<>();
 		ListaGenerica<Vertice<String>> vertices = grafo.listaDeVertices();
 		ListaGenerica<String> lista = new ListaEnlazadaGenerica<>();
-		ListaGenerica<Arista<String>> adyacentes = null;
-		int[] grados = new int[vertices.tamanio() + 1];
+		ListaGenerica<Arista<String>> aristas = null;
+		int[] gradosDeEntrada = new int[vertices.tamanio() + 1];
 		Vertice<String> vertice = null;
-		this.calcularGrados(grados, vertices);
-		apilarGradoCero(grados, pila, vertices);
-		while (!pila.esVacia()) {
-			vertice = pila.desapilar();
+		var numVerticesVisitados = 0;
+		this.calcularGradosDeEntrada(gradosDeEntrada, vertices);
+		encolarGradoCero(gradosDeEntrada, cola, vertices);
+		while (!cola.esVacia()) {
+			vertice = cola.desencolar();
 			lista.agregarFinal(vertice.dato());
-			grados[vertice.getPosicion()] = -1;
-			adyacentes = grafo.listaDeAdyacentes(vertice);
-			this.decrementarGradoYApilar(grados, adyacentes, pila);
+			++numVerticesVisitados;
+			gradosDeEntrada[vertice.getPosicion()] = -1;
+			aristas = grafo.listaDeAdyacentes(vertice);
+			this.decrementarGradoYEncolar(gradosDeEntrada, aristas, cola);
 		}
-		return lista;
+		if (numVerticesVisitados != vertices.tamanio()) {
+			throw new IllegalArgumentException("El grafo no es acíclico");
+		} else {
+			return lista;
+		}
 	}
 
 	public ListaGenerica<String> topsortEnProfundidad(Grafo<String> grafo) {
@@ -63,16 +76,17 @@ public class Topsort {
 		return lista;
 	}
 
-	private void topsortEnProfundidad(int i, Grafo<String> grafo, ListaGenerica<String> lista, PilaGenerica<Vertice<String>> pila, boolean[] visitados) {
-		ListaGenerica<Arista<String>> adyacentes = null;
+	private void topsortEnProfundidad(int i, Grafo<String> grafo, ListaGenerica<String> lista,
+			PilaGenerica<Vertice<String>> pila, boolean[] visitados) {
+		ListaGenerica<Arista<String>> aristas = null;
 		int j = 0;
 		visitados[i] = true;
 		Arista<String> arista = null;
 		Vertice<String> vertice = grafo.listaDeVertices().elemento(i);
-		adyacentes = grafo.listaDeAdyacentes(vertice);
-		adyacentes.comenzar();
-		while (!adyacentes.fin()) {
-			arista = adyacentes.proximo();
+		aristas = grafo.listaDeAdyacentes(vertice);
+		aristas.comenzar();
+		while (!aristas.fin()) {
+			arista = aristas.proximo();
 			j = arista.verticeDestino().getPosicion();
 			if (!visitados[j]) {
 				this.topsortEnProfundidad(j, grafo, lista, pila, visitados);
@@ -80,58 +94,57 @@ public class Topsort {
 		}
 		pila.apilar(vertice);
 	}
-
-	private void decrementarGradoYApilar(int[] grados, ListaGenerica<Arista<String>> adyacentes,
-			PilaGenerica<Vertice<String>> pila) {
+	
+	private void decrementarGradoYEncolar(int[] gradosDeEntrada, ListaGenerica<Arista<String>> aristas,
+			ColaGenerica<Vertice<String>> cola) {
 		Arista<String> arista;
-		adyacentes.comenzar();
-		while (!adyacentes.fin()) {
-			arista = adyacentes.proximo();
-			grados[arista.verticeDestino().getPosicion()]--;
-			if (grados[arista.verticeDestino().getPosicion()] == 0) {
-				pila.apilar(arista.verticeDestino());
+		aristas.comenzar();
+		while (!aristas.fin()) {
+			arista = aristas.proximo();
+			gradosDeEntrada[arista.verticeDestino().getPosicion()]--;
+			if (gradosDeEntrada[arista.verticeDestino().getPosicion()] == 0) {
+				cola.encolar(arista.verticeDestino());
 			}
 		}
-
 	}
 
-	private void apilarGradoCero(int[] grados, PilaGenerica<Vertice<String>> pila,
+	private void encolarGradoCero(int[] gradosDeEntrada, ColaGenerica<Vertice<String>> cola,
 			ListaGenerica<Vertice<String>> vertices) {
-		for (int i = 1; i < grados.length; i++) {
-			if (grados[i] == 0) {
-				pila.apilar(vertices.elemento(i));
+		for (int i = 1; i < gradosDeEntrada.length; i++) {
+			if (gradosDeEntrada[i] == 0) {
+				cola.encolar(vertices.elemento(i));
 			}
 		}
 	}
 
-	private void decrementarGrado(int[] grados, ListaGenerica<Arista<String>> adyacentes) {
+	private void decrementarGrado(int[] gradosDeEntrada, ListaGenerica<Arista<String>> aristas) {
 		Arista<String> arista;
-		adyacentes.comenzar();
-		while (!adyacentes.fin()) {
-			arista = adyacentes.proximo();
-			grados[arista.verticeDestino().getPosicion()]--;
+		aristas.comenzar();
+		while (!aristas.fin()) {
+			arista = aristas.proximo();
+			gradosDeEntrada[arista.verticeDestino().getPosicion()]--;
 		}
 	}
 
-	private void calcularGrados(int[] grados, ListaGenerica<Vertice<String>> vertices) {
+	private void calcularGradosDeEntrada(int[] gradosDeEntrada, ListaGenerica<Vertice<String>> vertices) {
+		ListaGenerica<Arista<String>> aristas;
 		Vertice<String> vertice;
 		Arista<String> arista;
-		ListaGenerica<Arista<String>> adyacentes;
 		vertices.comenzar();
 		while (!vertices.fin()) {
 			vertice = vertices.proximo();
-			adyacentes = ((VerticeImplListAdy<String>) vertice).obtenerAdyacentes();
-			adyacentes.comenzar();
-			while (!adyacentes.fin()) {
-				arista = adyacentes.proximo();
-				grados[arista.verticeDestino().getPosicion()]++;
+			aristas = ((VerticeImplListAdy<String>) vertice).obtenerAdyacentes();
+			aristas.comenzar();
+			while (!aristas.fin()) {
+				arista = aristas.proximo();
+				gradosDeEntrada[arista.verticeDestino().getPosicion()]++;
 			}
 		}
 	}
 
-	private int encontrarGradoCero(int[] grados) {
-		for (int i = 1; i < grados.length; i++) {
-			if (grados[i] == 0) {
+	private int encontrarGradoCero(int[] gradosDeEntrada) {
+		for (int i = 1; i < gradosDeEntrada.length; i++) {
+			if (gradosDeEntrada[i] == 0) {
 				return i;
 			}
 		}
